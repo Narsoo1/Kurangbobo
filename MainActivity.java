e168e197f8acf3847e7c15b1afaf111142e46132
vlamodix <duriahrealmenarzo@gmail.com>
2025-07-26 18:01:15 +0700

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,18 @@
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient()); // agar tidak buka browser
+        webView.loadUrl("https://kurangbobo.vercel.app/"); // ganti dengan web kamu
+        
+        setContentView(webView);
+    }
+}
